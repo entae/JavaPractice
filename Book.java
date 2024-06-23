5567ab16e74055a6007a06b3cbc1c6d4efffcf66
In Tae Chung <intae29@gmail.com>
2024-06-23 12:23:52 -0400

    completed Learning Java 17

---
+++ Book.java
@@ -0,0 +1,17 @@
+public class Book {
+    private String name;
+
+    public Book(String Name) {
+        this.name = Name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
+
+//// Test code
+//Answer answer = new Answer();
+//String bookName = "Best Software Developer Tips";
+//Answer.Book book = answer.new Book(bookName);
+//String result = book.getName();
